41b0942ff8d8a7fa66fb19bacd24e3e37d8b066c
NoctuG <s60985545@gmail.com>
2022-06-20 16:12:42 +0800

    更换主题
    Signed-off-by: NoctuG <s60985545@gmail.com>

--- scripts/WebManifestLoad.js
+++
@@ -1,5 +0,0 @@
-hexo.extend.injector.register(
-	'head_end',
-	'<link rel="manifest" href="/manifest.webmanifest">',
-	'default'
-);
---
+++ source/categories/index.md
@@ -0,0 +1,4 @@
+---
+title: categories
+date: 2022-06-20 15:28:59
+---
